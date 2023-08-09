4f43b9df31dfbed5814f11347c558e47f7e9e9c1
itc-s22029 <s22029@std.it-college.ac.jp>
2023-08-09 16:57:47 +0900

    chap8.1 commit

---
+++ app/src/main/java/jp/ac/it_college/std/s22029/menusample/MenuThanksActivity.kt
@@ -0,0 +1,28 @@
+package jp.ac.it_college.std.s22029.menusample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ac.it_college.std.s22029.menusample.databinding.ActivityMenuThanksBinding
+
+class MenuThanksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuThanksBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuThanksBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 前の画面から渡ってくるであろうデータを取り出す
+        val menuName = intent.getStringExtra("menuName") ?: ""
+        val menuPrice = intent.getIntExtra("menuPrice", 0)
+
+        // データをセット
+        binding.tvMenuName.text = menuName
+        binding.tvMenuPrice.text = "%,d".format(menuPrice)
+
+        // リストに戻るボタンをタップしたときの処理
+        binding.btThxBack.setOnClickListener {
+            finish()
+        }
+    }
+}
