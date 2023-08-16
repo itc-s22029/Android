c091c1b63256c57b73ddeb58926d2a696ee9f691
itc-s22029 <s22029@std.it-college.ac.jp>
2023-08-16 17:42:54 +0900

    chap08 commit

--- MenuSample/app/src/main/java/jp/ac/it_college/std/s22029/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/it_college/std/s22029/menusample/MainActivity.kt
@@ -16,8 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         // toolbar  をactionBarとして使うtoolbar
         setSupportActionBar(binding.toolbar)
 
